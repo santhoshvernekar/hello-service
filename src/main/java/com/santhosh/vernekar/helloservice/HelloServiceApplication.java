32883e79c073b0b7e6b970bfa2b34f3b1dce86cc
Santhosh <santoshmv121@gmail.com>
2021-02-11 00:26:20 +0530

    Initial Commit

---
+++ src/main/java/com/santhosh/vernekar/helloservice/HelloServiceApplication.java
@@ -0,0 +1,22 @@
+package com.santhosh.vernekar.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController //Bad idea to have on main class
+public class HelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String hello(@RequestHeader("x-location") String location){
+		return "Hello from: " + location;
+	}
+}
